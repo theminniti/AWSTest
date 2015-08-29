b0c9686c4cf7a2531ecbbb993ee7fd53966d5b1d
Chris Minniti <chrisminniti@Christophers-MacBook-Pro.local>
2015-08-29 16:54:00 -0400

    json try 4

--- read.php
+++ read.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, food FROM Test";
+$sql = mysql_query("SELECT id, food FROM Test");
 
 $results = array();
 while($row = mysql_fetch_array($sql))
